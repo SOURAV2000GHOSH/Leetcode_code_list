826ffe33220ec68830c15cd31194c049796bad2a
Sourav Ghosh <souravghosh6543@gmail.com>
2022-08-26 23:27:16 +0530

    Time: 20 ms (21.10%), Space: 44.8 MB (45.68%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int a=x;
+        int temp=0;
+        int rem;
+        while(a>0){
+            rem=a%10;
+            temp=(temp*10)+rem;
+            a=(int)a/10;
+            
+        }
+        if(temp==x){
+            return true;
+        }
+        return false;
+    }
+}
