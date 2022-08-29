500fabf9ceff88624ac8671080cbe0df412f55e6
Sourav Ghosh <souravghosh6543@gmail.com>
2022-08-29 20:39:40 +0530

    Time: 70 ms (62.03%), Space: 13.8 MB (76.15%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -2,15 +2,14 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         a={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         sum=0
-        l=[]
-        for x in s:
-            l.append(x)
+        #"IIIVSKJKSJSQSQSBQSLNQL"
+        
         x=0
-        while x<len(l):
-            if x<len(l)-1 and a[l[x]]<a[l[x+1]]:
-                sum += (a[l[x+1]] - a[l[x]])
+        while x<len(s):
+            if x<len(s)-1 and a[s[x]]<a[s[x+1]]:
+                sum += (a[s[x+1]] - a[s[x]])
                 x += 2
             else:
-                sum += a[l[x]]
+                sum += a[s[x]]
                 x += 1
         return sum
