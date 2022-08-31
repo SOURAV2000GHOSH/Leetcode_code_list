6689ea170a8049816dcf9ed935accddb9a438bad
Sourav Ghosh <souravghosh6543@gmail.com>
2022-08-31 12:43:08 +0530

    Time: 51 ms (44.35%), Space: 14 MB (9.39%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.py
@@ -0,0 +1,30 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        cheakcapital=set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        cheaksmall=set("abcdefghijklmnopqrstuvwxyz")
+        if word[0] in cheakcapital:
+            issmall=False
+            for x in range(1,len(word)):
+                if word[x] not in cheakcapital:
+                    if word[x] in cheaksmall:
+                        issmall=True
+                        continue
+                elif issmall:
+                    return False
+            iscap=False
+            for x in range(1,len(word)):
+                if word[x] not in cheaksmall:
+                    if word[x] in cheakcapital:
+                        iscap=True
+                        continue
+                elif iscap:
+                    return False
+        else:
+            for x in range(1,len(word)):
+                if word[x] in cheakcapital:
+                    return False
+            
+        return True
+            
+                    
+                
