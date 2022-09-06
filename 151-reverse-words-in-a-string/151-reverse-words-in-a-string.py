91916c7f4451939522559228d5b6423a0fafce0f
Sourav Ghosh <souravghosh6543@gmail.com>
2022-09-06 23:10:33 +0530

    Time: 71 ms (18.60%), Space: 14 MB (80.50%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        ans=[]
+        cheak=set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890")
+        l=len(s)
+        i=l-1
+        while True:
+            if s[i] not in cheak:
+                i-=1
+            break
+        j=None
+        while i>-1:
+            if s[i] not in cheak:
+                i-=1
+                continue
+            j=i+1
+            while True:
+                if i>-1 and s[i] in cheak:
+                    i-=1
+                    continue
+                break
+            ans.append(s[i+1:j])
+            
+        return " ".join(ans)
+            
+        
+                    
+                
+            
+        
