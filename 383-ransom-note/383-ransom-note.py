5c4ba7c6f6881294a5f301959ebbe0f7dc5b39e3
Sourav Ghosh <souravghosh6543@gmail.com>
2022-09-12 00:17:53 +0530

    Time: 70 ms (76.14%), Space: 14.1 MB (93.80%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,5 +1,7 @@
 class Solution:
     def canConstruct(self, r: str, m: str) -> bool:
+        if len(r)>len(m):
+            return False
         a=set(r)
         for x in a:
             if r.count(x) > m.count(x):
