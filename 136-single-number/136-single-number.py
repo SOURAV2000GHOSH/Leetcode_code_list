008bfe5df5b6568b2addeef60df7dd83844ff933
Sourav Ghosh <souravghosh6543@gmail.com>
2022-09-13 12:57:14 +0530

    Time: 316 ms (20.15%), Space: 16.9 MB (19.36%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,7 +1,20 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        a=0
-        for x in nums:
-            a= a^x
-        return a
-        
+        if len(nums)==1:
+            return nums[0]
+        nums.sort()
+        l=0
+        r=len(nums)-1
+        if nums[r]!=nums[r-1]:
+            return nums[r]
+        if nums[l] != nums[l+1]:
+            return nums[l]
+        while l<=r:
+            mid=l+(r-l)//2
+            if nums[mid-1] != nums[mid] != nums[mid+1]:
+                return nums[mid]
+            if (nums[mid]!=nums[mid+1] and mid%2==1) or (mid%2==0 and nums[mid] != nums[mid-1]):
+                l=mid+1
+            else:
+                r=mid-1
+            
