e1e62ca8cf589785c3b069438033dfc7fabf86df
Sourav Ghosh <souravghosh6543@gmail.com>
2022-10-08 10:10:19 +0530

    Time: 3644 ms (5.21%), Space: 30.4 MB (95.95%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,23 +1,6 @@
 class Solution(object):
     def pivotArray(self, nums, pivot):
-        temp=[]
-        for x in nums:
-            temp.append(x)
-        l=len(temp)
-        i=0
-        for x in temp:
-            if x<pivot:
-                nums[i]=x
-                i+=1
-        for x in temp:
-            if x==pivot:
-                nums[i]=x
-                i+=1
-        for x in temp:
-            if x>pivot:
-                nums[i]=x
-                i+=1
-        return nums
+        return [x for x in nums if x<pivot]+[x for x in nums if x==pivot]+[x for x in nums if x>pivot]
             
             
         
