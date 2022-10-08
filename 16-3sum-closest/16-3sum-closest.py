4cd5da723c361b5bb18ef31f8be91244308e304f
Sourav Ghosh <souravghosh6543@gmail.com>
2022-10-08 09:06:22 +0530

    Time: 8639 ms (14.45%), Space: 13.5 MB (78.60%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,22 +1,23 @@
-class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        int n=nums.length, result=0,diff=Integer.MAX_VALUE;
-        Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            int s=i+1,e=n-1;
-            while(s<e){
-                int sum=nums[i]+nums[s]+nums[e];
-                if(Math.abs(diff)>Math.abs(sum-target)){
-                    diff=sum-target;
-                    result=sum;
-                }
-                if(sum<target)
-                    s++;
-                else
-                    e--;
-            }
-        }
-        return result;
-        
-    }
-}
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        res = sum(nums[:3])
+        for i in xrange(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = sum((nums[i], nums[l], nums[r]))
+                if abs(s-target) < abs(res-target):
+                    res = s
+                if s < target:
+                    l += 1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+                elif s > target:
+                    r -= 1
+                    while l<r and nums[r]==nums[r+1]:
+                        r-=1
+                else: 
+                    return res
+        return res
