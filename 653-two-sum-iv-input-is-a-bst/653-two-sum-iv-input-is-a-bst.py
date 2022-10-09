7044c5a93eb9b52b7cc3b953f58b0f56424c3f5c
Sourav Ghosh <souravghosh6543@gmail.com>
2022-10-09 07:48:01 +0530

    Time: 179 ms (17.86%), Space: 20.7 MB (5.58%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -1,38 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        l,r=self.dll(root)
-        while l.val != r.val:
-            temp=l.val+r.val
-            if temp >k:
-                r=r.left
-            elif temp < k:
-                l=l.right
-            else:
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        temp=set()
+        def inorder(root):
+            if root:
+                inorder(root.left)
+                temp.add(root.val)
+                inorder(root.right)
+        inorder(root)
+        while len(temp)>0:
+            cheak=k-temp.pop()
+            if cheak in temp:
                 return True
         return False
-    def dll(self,root):
-        if not root:
-            return None,None
-        lh,lt=self.dll(root.left)
-        rh,rt=self.dll(root.right)
-        if lh:
-            l=lh
-            lt.right=root
-            root.left=lt
-        else:
-            l=root
-        if rh:
-            r=rt
-            root.right=rh
-            rh.left=root
-        else:
-            r=root
-        return l,r
-        
         
