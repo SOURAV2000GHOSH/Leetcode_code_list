c7a4a2d4d54e240227db62dab96ffea0ebf2f73d
Sourav Ghosh <souravghosh6543@gmail.com>
2022-10-14 18:00:29 +0530

    Time: 395 ms (72.71%), Space: 40.5 MB (5.65%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -2,12 +2,11 @@ class MyHashMap:
 
     def __init__(self):
         self.MyHashMap=[-1]*(10**6+1)
-        self.cheak=set()
         
 
     def put(self, key: int, value: int) -> None:
         self.MyHashMap[key]=value
-        self.cheak.add(key)
+
         
 
     def get(self, key: int) -> int:
@@ -15,9 +14,8 @@ class MyHashMap:
         
 
     def remove(self, key: int) -> None:
-        if key in self.cheak:
-            self.MyHashMap[key]=-1
-            self.cheak.remove(key)
+        self.MyHashMap[key]=-1
+            
             
 
 
