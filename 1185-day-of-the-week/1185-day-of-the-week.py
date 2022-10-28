e8b7100fad6ab40f155ec8cd01e1ab552e70cc32
Sourav Ghosh <souravghosh6543@gmail.com>
2022-10-28 13:12:01 +0530

    Time: 34 ms (90.26%), Space: 14 MB (28.57%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
+        dates = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        # this is a unique number code
+        t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
+        y = year
+        m = month
+        if m < 3:
+            y -= 1
+        # this is a important folmula
+        return dates[((y + int(y / 4) - int(y / 100) + int(y / 400) + t[m - 1] + day) % 7)]
+
