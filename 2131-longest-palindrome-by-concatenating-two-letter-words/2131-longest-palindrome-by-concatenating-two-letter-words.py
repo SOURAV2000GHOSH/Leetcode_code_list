55a0ba8a2e4be7052c665ee206d95cc0929ff6f0
Sourav Ghosh <souravghosh6543@gmail.com>
2022-11-03 07:45:27 +0530

    Time: 3325 ms (37.52%), Space: 38.4 MB (54.38%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,15 +1,19 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
+        #check is use for storing the number of sequence and cheaking
         check=dict()
+        #iteration_cheak is use for not
+        iteration_cheak=set()
         ans=0
         mid=0
-        iteration_cheak=set()
         for x in words:
             check[x]=1 if x not in check else check[x]+1
             
         for key in check.keys():
+            if key in iteration_cheak:
+                continue
             temp_key=key[::-1]
-            if key==temp_key and temp_key in check and key not in iteration_cheak:
+            if key==temp_key and temp_key in check:
                 if check[key]%2==1:
                     mid=2
                 if check[key]>1:
@@ -18,7 +22,7 @@ class Solution:
                     else:
                         ans+=(check[key]-1)*2
                 
-            elif temp_key in check and key not in iteration_cheak:
+            elif temp_key in check:
                 ans+=min(check[key],check[temp_key])*4
             iteration_cheak.add(temp_key)
         return ans+mid
