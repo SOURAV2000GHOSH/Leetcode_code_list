3f4b219a1f84cce7db2dc84b97c0704ad2bfe7fa
Sourav Ghosh <souravghosh6543@gmail.com>
2022-11-05 09:35:49 +0530

    Time: 72 ms (88.84%), Space: 14 MB (37.20%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans=[]
+        def calculate(indx,n,arr,total,target):
+            if indx>=n:
+                return
+            if total==target:
+                temp=[]
+                for x in arr:
+                    temp.append(x)
+                ans.append(temp)
+                return
+            elif total>target:
+                return
+            arr.append(candidates[indx])
+            total+=candidates[indx]
+            calculate(indx,n,arr,total,target)
+            arr.pop()
+            total-=candidates[indx]
+            calculate(indx+1,n,arr,total,target)
+        calculate(0,len(candidates),[],0,target)
+
+        return ans
