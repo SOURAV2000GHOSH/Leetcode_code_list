6bebf1d2a236e8cb357635dcecb48125f102ba52
Sourav Ghosh <souravghosh6543@gmail.com>
2022-11-07 13:48:08 +0530

    Time: 33 ms (92.07%), Space: 13.9 MB (9.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,20 +1,8 @@
 class Solution:
-    # def makeNum(arr):
-    #     num=0
-    #     for x in arr:
-    #         num=(num*10)+x
-    #     return num
     def maximum69Number (self, num: int) -> int:
-        max_num=[num]
         temp=str(num)
         for i in range(len(temp)):
             if temp[i]=='6':
                 temp_num=temp[:i]+'9'+temp[i+1:]
-                if int(temp_num)>max_num[0]:
-                    max_num[0]=int(temp_num)
-                    break
-        return max_num[0]
-        
-            
-            
-        
+                return int(temp_num)
+        return num
