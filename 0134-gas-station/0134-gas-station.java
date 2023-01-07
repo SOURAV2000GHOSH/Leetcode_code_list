b98c75e55ebc02fc1c19c3c352bca08bbca1171f
Sourav Ghosh <souravghosh6543@gmail.com>
2023-01-07 23:14:42 +0530

    Time: 6 ms (46.17%), Space: 62.8 MB (64.94%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,14 +1,17 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n=gas.length;
-        int totalGas=0,totalCost=0;
-        for(int i=0;i<n;i++){
-            totalGas+=gas[i];
-            totalCost+=cost[i];
-        }
+        int totalGas=Arrays.stream(gas).sum();
+        int totalCost=Arrays.stream(cost).sum();
+        //if cost of all station is greater than sum of gas then we can cont complete all station that's why we return -1;
+        // for(int i=0;i<n;i++){
+        //     totalGas+=gas[i];
+        //     totalCost+=cost[i];
+        // }
         if(totalCost>totalGas){
             return -1;
         }
+        //if get 0 or positive total in any index then we get answer;
         int result=0;
         int total=0;
         for(int i=0;i<n;i++){
@@ -17,7 +20,6 @@ class Solution {
                     total=0;
                     result=i+1;
                 }
-            
         }
         return result;
     }
