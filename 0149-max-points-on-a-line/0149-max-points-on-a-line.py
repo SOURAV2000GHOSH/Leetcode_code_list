9a74a356602b472146a837d087a95192d8646d8d
Sourav Ghosh <souravghosh6543@gmail.com>
2023-01-09 00:36:10 +0530

    Time: 108 ms (81.17%), Space: 14.3 MB (40.65%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -4,15 +4,18 @@ class Solution:
             return 1
         result=0
         for i in range(len(points)):
-            for j in range(i+1,len(points)):
+            check=dict()
+            for j in range(len(points)):
+                if j==i:
+                    continue
                 dy=(points[j][1]-points[i][1])
                 dx=(points[j][0]-points[i][0])
-                count=2
-                for k in range(len(points)):
-                    if(k!=i and k!=j):
-                        dy_=(points[k][1]-points[i][1])
-                        dx_=(points[k][0]-points[i][0])
-                        if((dy*dx_)==(dy_*dx)):
-                            count += 1
-                result=max(result,count)
-        return result
+                thita = math.atan2(dy,dx)
+                if thita in check:
+                    check[thita]+=1
+                else:
+                    check[thita]=1
+                result=max(result,check[thita])
+            # for value in check.values():
+            #     result=max(result,value)
+        return result+1
