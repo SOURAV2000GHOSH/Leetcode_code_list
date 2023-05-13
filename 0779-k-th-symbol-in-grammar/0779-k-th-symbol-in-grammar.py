57238d4ead26806ff857284ae8fe1d3cced71971
Sourav Ghosh <souravghosh6543@gmail.com>
2023-05-13 11:45:20 +0530

    Time: 50 ms (5.20%), Space: 16.3 MB (13.41%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -3,17 +3,25 @@ class Solution:
         if n==1 and k==1:
             return 0
         temp=1
-        if k%2==0:
-            temp=self.kthGrammar(n-1,k//2)
+        # if k%2==0:
+        #     temp=self.kthGrammar(n-1,k//2)
+        # else:
+        #     temp=self.kthGrammar(n-1,(k//2)+1)
+        
+        if k<=(2**(n-1)//2):
+            temp=self.kthGrammar(n-1,k)
         else:
-            temp=self.kthGrammar(n-1,(k//2)+1)
-        if temp==0:
-            if k%2==1:
-                return 0
-            else:
-                return 1
-        else:
-            if k%2==1:
-                return 1
-            else:
-                return 0
+            temp=0 if self.kthGrammar(n-1,k-(2**(n-1)//2))==1 else 1
+            
+        # if temp==0:
+        #     if k%2==1:
+        #         return 0
+        #     else:
+        #         return 1
+        # else:
+        #     if k%2==1:
+        #         return 1
+        #     else:
+        #         return 0
+        
+        return temp
