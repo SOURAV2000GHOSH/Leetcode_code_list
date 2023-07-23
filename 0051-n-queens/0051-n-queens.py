e29a787b89d9a00c44f8177f9231b8209f470d13
Sourav Ghosh <souravghosh6543@gmail.com>
2023-07-23 20:43:56 +0530

    Time: 130 ms (19.09%), Space: 16.8 MB (81.74%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isSafe(self,row,col,board,n):
+        drow=row
+        dcol=col
+        while drow>=0 and dcol>=0:
+            if board[drow][dcol]=="Q":
+                return False
+            drow-=1
+            dcol-=1
+        drow=row
+        dcol=col
+        while drow>=0 and dcol>=0:
+            if board[drow][dcol]=="Q":
+                return False
+            dcol-=1
+        drow=row
+        dcol=col
+        while n>drow>=0 and dcol>=0:
+            if board[drow][dcol]=="Q":
+                return False
+            drow+=1
+            dcol-=1
+        return True
+    
+    def traverse(self,col,n,board,ans):
+        if col==n:
+            temp=[]
+            for el in board:
+                temp.append("".join(el))
+            ans.append(temp)
+            return
+        for row in range(n):
+            if self.isSafe(row,col,board,n):
+                board[row][col]="Q"
+                self.traverse(col+1,n,board,ans)
+                board[row][col]="."
+        
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans=[]
+        board=[["." for i in range(n)] for j in range(n)]
+        self.traverse(0,n,board,ans)
+        return ans
+        
