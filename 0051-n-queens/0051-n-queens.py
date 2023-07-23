96f72e0d487047ff0e9e24e6746873ac9846b311
Sourav Ghosh <souravghosh6543@gmail.com>
2023-07-23 23:08:23 +0530

    Time: 54 ms (98.43%), Space: 16.8 MB (81.74%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isSafe(self,row,col,board,n):
+    def isSafe(self,row,col,board,n,NEdiagonal,SEdiagonal,Erow):
         drow=row
         dcol=col
         while drow>=0 and dcol>=0:
@@ -22,7 +22,7 @@ class Solution:
             dcol-=1
         return True
     
-    def traverse(self,col,n,board,ans):
+    def traverse(self,col,n,board,ans,NEdiagonal,SEdiagonal,Erow):
         if col==n:
             temp=[]
             for el in board:
@@ -30,14 +30,23 @@ class Solution:
             ans.append(temp)
             return
         for row in range(n):
-            if self.isSafe(row,col,board,n):
+            if not Erow[row] and not NEdiagonal[(n-1)+(col-row)] and not SEdiagonal[row+col]:
                 board[row][col]="Q"
-                self.traverse(col+1,n,board,ans)
+                Erow[row]=True
+                NEdiagonal[(n-1)+(col-row)]=True
+                SEdiagonal[row+col]=True
+                self.traverse(col+1,n,board,ans,NEdiagonal,SEdiagonal,Erow)
+                Erow[row]=False
+                NEdiagonal[(n-1)+(col-row)]=False
+                SEdiagonal[row+col]=False
                 board[row][col]="."
         
     def solveNQueens(self, n: int) -> List[List[str]]:
+        NEdiagonal=[False]*((2*n)-1)
+        SEdiagonal=[False]*((2*n)-1)
+        Erow=[False]*n
         ans=[]
         board=[["." for i in range(n)] for j in range(n)]
-        self.traverse(0,n,board,ans)
+        self.traverse(0,n,board,ans,NEdiagonal,SEdiagonal,Erow)
         return ans
         
