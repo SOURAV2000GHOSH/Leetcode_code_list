3e92a86affdcc8816645940c59e6374f30f0c754
Sourav Ghosh <souravghosh6543@gmail.com>
2023-07-23 23:10:43 +0530

    Time: 52 ms (92.01%), Space: 16.4 MB (24.43%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,40 +1,26 @@
 class Solution:
-    def isSafe(self,row,col,board,n):
-        drow=row
-        dcol=col
-        while drow>=0 and dcol>=0:
-            if board[drow][dcol]=="Q":
-                return False
-            drow-=1
-            dcol-=1
-        drow=row
-        dcol=col
-        while drow>=0 and dcol>=0:
-            if board[drow][dcol]=="Q":
-                return False
-            dcol-=1
-        drow=row
-        dcol=col
-        while n>drow>=0 and dcol>=0:
-            if board[drow][dcol]=="Q":
-                return False
-            drow+=1
-            dcol-=1
-        return True
-    
-    def traverse(self,col,n,board,ans):
+    def traverse(self,col,n,board,ans,NEdiagonal,SEdiagonal,Erow):
         if col==n:
             ans[0]+=1
             return
         for row in range(n):
-            if self.isSafe(row,col,board,n):
+            if not Erow[row] and not NEdiagonal[(n-1)+(col-row)] and not SEdiagonal[row+col]:
                 board[row][col]="Q"
-                self.traverse(col+1,n,board,ans)
+                Erow[row]=True
+                NEdiagonal[(n-1)+(col-row)]=True
+                SEdiagonal[row+col]=True
+                self.traverse(col+1,n,board,ans,NEdiagonal,SEdiagonal,Erow)
+                Erow[row]=False
+                NEdiagonal[(n-1)+(col-row)]=False
+                SEdiagonal[row+col]=False
                 board[row][col]="."
         
     def totalNQueens(self, n: int) -> List[List[str]]:
+        NEdiagonal=[False]*((2*n)-1)
+        SEdiagonal=[False]*((2*n)-1)
+        Erow=[False]*n
         ans=[0]
         board=[["." for i in range(n)] for j in range(n)]
-        self.traverse(0,n,board,ans)
+        self.traverse(0,n,board,ans,NEdiagonal,SEdiagonal,Erow)
         return ans[0]
         
