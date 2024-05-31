02641df59dd3e8cb3a4d121caeae43f7a79a4750
Sourav Ghosh <souravghosh6543@gmail.com>
2024-05-31 23:58:58 +0530

    Time: 1 ms (100.00%), Space: 45.6 MB (36.75%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,13 +1,19 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
+        //getting XOR value of two unique values
         int temp=0;
         for(int n:nums){
             temp=temp^n;
         }
+        
+        // getting the bit that will differenciate between that two numbers 
         int bit=1;
         while((temp&bit)==0){
             bit=(bit<<1);
         }
+        
+        /*based on that bits dividing whole array into two parts 
+        and get two unique value*/
         int temp1=0,temp2=0;
         for(int n:nums){
             if((n&bit)==0){
@@ -16,6 +22,7 @@ class Solution {
                 temp2=temp2^n;
             }
         }
+        
         int ans[]={temp1,temp2};
         return ans;
     }
