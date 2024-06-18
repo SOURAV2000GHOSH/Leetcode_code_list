fa479213d27c4181c9977642c455defddb90ffdb
Sourav Ghosh <souravghosh6543@gmail.com>
2024-06-18 22:44:43 +0530

    Time: 22 ms (49.14%), Space: 45 MB (97.30%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,18 +1,22 @@
+//use for storing purposes
 class Node{
-    int dif,profit;
-    public Node(int dif,int profit){
-        this.dif=dif;
+    int diference,profit;
+    public Node(int diference,int profit){
+        this.diference=diference;
         this.profit=profit;
     }
 }
+
 class Solution {
     public int solve(List<Node> store,int ind){
         int ans=0;
         int start=0,end=store.size()-1;
+        /*doing binary search to get at that 
+        index worker can work and we will get max profit*/
         while(start<=end){
             int mid=start-((start-end)/2);
             Node temp=store.get(mid);
-            if(temp.dif<=ind){
+            if(temp.diference<=ind){
                 ans=temp.profit;
                 start=mid+1;
             }else{
@@ -28,13 +32,15 @@ class Solution {
         for(int i=0;i<n;i++){
             store.add(new Node(difficulty[i],profit[i]));
         }
-        Collections.sort(store,(a,b)->a.dif-b.dif);
-        int maxProfit=0;
-        for(int i=0;i<store.size();i++){
-            Node temp=store.get(i);
-            maxProfit=Math.max(temp.profit,maxProfit);
-            temp.profit=maxProfit;
+        //sorting based on difference
+        Collections.sort(store,(a,b)->a.diference-b.diference);
+        //use prefix sum to store maximum profit at that index from starting
+        for(int i=1;i<store.size();i++){
+            int maxProfit=Math.max(store.get(i).profit,store.get(i-1).profit);
+            store.get(i).profit=maxProfit;
         }
+        
+        //calculating answer
         int ans=0;
         for(int x:worker){
             int temp=solve(store,x);
