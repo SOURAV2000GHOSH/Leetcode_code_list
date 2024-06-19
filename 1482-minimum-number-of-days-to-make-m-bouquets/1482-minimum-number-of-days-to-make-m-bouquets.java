f58240d5a35755b25b5f92989df9ca47d5b3fb62
Sourav Ghosh <souravghosh6543@gmail.com>
2024-06-19 23:03:56 +0530

    Time: 16 ms (95.37%), Space: 57.9 MB (42.55%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,8 +1,8 @@
 class Solution {
+    //traversing all the bloomDay[] for single day
     public boolean traverse(int day,int []bloomDay,int m,int k){
-        int i=0,count=0,n=bloomDay.length;
-        int adjCount=0;
-        while(i<n){
+        int i=0,count=0,adjCount=0;
+        while(i<bloomDay.length){
             if(bloomDay[i]>day){
                 adjCount=0;
             }else{
@@ -12,14 +12,17 @@ class Solution {
                 count++;
                 adjCount=0;
             }
+            i++;
+            /*if needed count will get at time of traversing 
+            that time no need to check further*/
             if(count==m)
                 return true;
-            i++;
         }
         if(count>=m)
             return true;
         return false;
     }
+    
     public int minDays(int[] bloomDay, int m, int k) {
         int n=bloomDay.length;
         int start=1,end=0;
@@ -27,6 +30,7 @@ class Solution {
             end=Math.max(end,x);
         }
         int ans=-1;
+        //using binary search to get the ans in O(logN) time
         while(start<=end){
             int mid=start-((start-end)/2);
             if(traverse(mid,bloomDay,m,k)){
