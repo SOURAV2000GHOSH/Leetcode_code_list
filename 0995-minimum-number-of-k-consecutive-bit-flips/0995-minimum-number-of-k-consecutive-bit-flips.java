f0776d51080e446cb89d7bf5231e4af8230f24ac
Sourav Ghosh <souravghosh6543@gmail.com>
2024-06-24 22:28:26 +0530

    Time: 5 ms (88.39%), Space: 54.6 MB (98.71%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
@@ -1,15 +1,14 @@
 class Solution {
     public int minKBitFlips(int[] nums, int k) {
         int curFlip=0,totalFlip=0,n=nums.length;
-        boolean isFlip[]=new boolean[n];
         for(int i=0;i<n;i++){
-            if(i>=k && isFlip[i-k]){
+            if(i>=k && nums[i-k]==-1){
                 curFlip--;
             }
             if(curFlip%2 == nums[i]){
                 if(i+k>n)
                     return -1;
-                isFlip[i]=true;
+                nums[i]=-1;
                 curFlip++;
                 totalFlip++;
             }
