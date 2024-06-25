ed7d228c10d028d3eda1166ad79d0c0df46aa2d0
Sourav Ghosh <souravghosh6543@gmail.com>
2024-06-25 09:44:14 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (68.60%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -15,19 +15,24 @@
  */
 class Solution {
     public void traverse(TreeNode root,int []arr){
+        //add value of leaf node to store
         if(root.left==null && root.right==null){
             arr[0]+=root.val;
             root.val=arr[0];
             return;
         }
+        // goint to right sub tree
         if(root.right!=null){
             traverse(root.right,arr);
         }
+        //adding cur node data that would be use for left sub tree
         arr[0]+=root.val;
         root.val=arr[0];
+        //goint to left sub tree
         if(root.left!=null){
             traverse(root.left,arr);
         }
+        
     }
     public TreeNode convertBST(TreeNode root) {
         if(root==null){
